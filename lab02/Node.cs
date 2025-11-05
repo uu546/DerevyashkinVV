2a7a36b55613c53f725e629766c8316018c3e2c8
uu <uu546@mail.ru>
2025-11-05 17:43:00 +0300

    Добавить класс узла

---
+++ lab02/Node.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// Класс узла связного списка.
+/// </summary>
+public class Node
+{
+    public int Value;         // O(1)
+
+    public Node? Next;    // O(1)
+
+    public Node(int value)   
+    {
+        Value = value;      // O(1)
+        Next = null;        // O(1)
+    }
+}
