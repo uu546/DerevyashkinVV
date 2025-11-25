b518b3c6b34a58011b7a333515061e69af6db79f
uu <uu546@mail.ru>
2025-11-25 23:14:40 +0300

    Добавить юнит тест для метода цепочек

---
+++ lab05/lab05.Tests/ChainingGetTest.cs
@@ -0,0 +1,26 @@
+﻿public class ChainingGetTest
+{
+    [Fact]
+    public void GetTest()
+    {
+        HashTableChaining hashTableChaining = new HashTableChaining(2);
+
+        hashTableChaining.Add("key1", "value1");
+        hashTableChaining.Add("key2", "value2");
+        hashTableChaining.Add("key3", "value3");
+
+        Assert.Equal("value3", hashTableChaining.Get("key3"));
+    }
+
+    [Fact]
+    public void GetNotEqualTest()
+    {
+        HashTableChaining hashTableChaining = new HashTableChaining(2);
+
+        hashTableChaining.Add("key1", "value1");
+        hashTableChaining.Add("key2", "value2");
+        hashTableChaining.Add("key3", "value3");
+
+        Assert.NotEqual("value1", hashTableChaining.Get("key3"));
+    }
+}
