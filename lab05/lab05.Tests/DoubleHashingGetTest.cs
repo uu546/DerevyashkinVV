34e3963894eb80d2d19c99f115d23279388a7c03
uu <uu546@mail.ru>
2025-11-25 23:15:03 +0300

    Добавить юнит тест для метода двойного хеширования

---
+++ lab05/lab05.Tests/DoubleHashingGetTest.cs
@@ -0,0 +1,26 @@
+﻿public class DoubleHashingGetTest
+{
+    [Fact]
+    public void GetTest()
+    {
+        HashTableDoubleHashing hashTableDoubleHashing = new HashTableDoubleHashing(2);
+
+        hashTableDoubleHashing.Add("key1", "value1");
+        hashTableDoubleHashing.Add("key2", "value2");
+        hashTableDoubleHashing.Add("key3", "value3");
+
+        Assert.Equal("value3", hashTableDoubleHashing.Get("key3"));
+    }
+
+    [Fact]
+    public void GetNotEqualTest()
+    {
+        HashTableDoubleHashing hashTableDoubleHashing = new HashTableDoubleHashing(2);
+
+        hashTableDoubleHashing.Add("key1", "value1");
+        hashTableDoubleHashing.Add("key2", "value2");
+        hashTableDoubleHashing.Add("key3", "value3");
+
+        Assert.NotEqual("value1", hashTableDoubleHashing.Get("key3"));
+    }
+}
