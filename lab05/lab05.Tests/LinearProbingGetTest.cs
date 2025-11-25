e205ddfa073f550ee2e9776263ffeb89b83fb61c
uu <uu546@mail.ru>
2025-11-25 23:15:13 +0300

    Добавить юнит тест для метода линейного пробирования

---
+++ lab05/lab05.Tests/LinearProbingGetTest.cs
@@ -0,0 +1,26 @@
+﻿public class LinearProbingGetTest
+{
+    [Fact]
+    public void GetTest()
+    {
+        HashTableLinearProbing hashTableLinearProbing = new HashTableLinearProbing(2);
+
+        hashTableLinearProbing.Add("key1", "value1");
+        hashTableLinearProbing.Add("key2", "value2");
+        hashTableLinearProbing.Add("key3", "value3");
+
+        Assert.Equal("value3", hashTableLinearProbing.Get("key3"));
+    }
+
+    [Fact]
+    public void GetNotEqualTest()
+    {
+        HashTableLinearProbing hashTableLinearProbing = new HashTableLinearProbing(2);
+
+        hashTableLinearProbing.Add("key1", "value1");
+        hashTableLinearProbing.Add("key2", "value2");
+        hashTableLinearProbing.Add("key3", "value3");
+
+        Assert.NotEqual("value1", hashTableLinearProbing.Get("key3"));
+    }
+}
